1fd9717372ef3d3556e319d86641e3271d0a87ba
MohammadALSalamat <alomda.alslmat@gmail.com>
2022-05-01 17:50:59 +0400

    updates

--- resources/views/frontend/frontend_pages/products/shop.blade.php
+++ resources/views/frontend/frontend_pages/products/shop.blade.php
@@ -26,9 +26,11 @@
                             Now<i class="w-icon-long-arrow-right"></i></a>
                     </div>
                 </div>
-                <!-- End of Shop Banner -->
-                <div class="shop-default-category category-ellipse-section mb-6">
-                    <div class="swiper-container swiper-theme shadow-swiper"
+                
+                <div class="container-fluid">
+                    <!-- End of Shop Banner -->
+                <div class="shop-default-category category-ellipse-section mb-6"  style="height: auto !important">
+                    <div class="swiper-container swiper-theme shadow-swiper" style="height: auto !important"
                         data-swiper-options="{
                         'spaceBetween': 20,
                         'slidesPerView': 2,
@@ -51,7 +53,7 @@
                             }
                         }
                     }">
-                        <div class="swiper-wrapper row gutter-lg cols-xl-8 cols-lg-7 cols-md-6 cols-sm-4 cols-xs-3 cols-2">
+                        <div  style="height: auto !important" class="swiper-wrapper row gutter-lg cols-xl-8 cols-lg-7 cols-md-6 cols-sm-4 cols-xs-3 cols-2">
                             <div class="swiper-slide category-wrap">
                                 <div class="category category-ellipse">
                                     <figure class="category-media">
@@ -71,7 +73,7 @@
                                 <div class="category category-ellipse">
                                     <figure class="category-media">
                                         <a href="shop-banner-sidebar.html">
-                                            <img src="assets/images/categories/category-5.jpg" alt="Categroy"
+                                            <img src="{{ asset('front-style/assets/images/categories/category-5.jpg') }}" alt="Categroy"
                                                 width="190" height="190" style="background-color: #B8BDC1;" />
                                         </a>
                                     </figure>
@@ -86,7 +88,7 @@
                                 <div class="category category-ellipse">
                                     <figure class="category-media">
                                         <a href="shop-banner-sidebar.html">
-                                            <img src="assets/images/categories/category-6.jpg" alt="Categroy"
+                                            <img src="{{ asset('front-style/assets/images/categories/category-6.jpg') }}" alt="Categroy"
                                                 width="190" height="190" style="background-color: #99C4CA;" />
                                         </a>
                                     </figure>
@@ -101,7 +103,7 @@
                                 <div class="category category-ellipse">
                                     <figure class="category-media">
                                         <a href="shop-banner-sidebar.html">
-                                            <img src="assets/images/categories/category-7.jpg" alt="Categroy"
+                                            <img src="{{ asset('front-style/assets/images/categories/category-7.jpg') }}" alt="Categroy"
                                                 width="190" height="190" style="background-color: #4E5B63;" />
                                         </a>
                                     </figure>
@@ -116,7 +118,7 @@
                                 <div class="category category-ellipse">
                                     <figure class="category-media">
                                         <a href="shop-banner-sidebar.html">
-                                            <img src="assets/images/categories/category-8.jpg" alt="Categroy"
+                                            <img src="{{ asset('front-style/assets/images/categories/category-8.jpg') }}" alt="Categroy"
                                                 width="190" height="190" style="background-color: #D3E5EF;" />
                                         </a>
                                     </figure>
@@ -131,7 +133,7 @@
                                 <div class="category category-ellipse">
                                     <figure class="category-media">
                                         <a href="shop-banner-sidebar.html">
-                                            <img src="assets/images/categories/category-9.jpg" alt="Categroy"
+                                            <img src="{{ asset('front-style/assets/images/categories/category-9.jpg') }}" alt="Categroy"
                                                 width="190" height="190" style="background-color: #65737C;" />
                                         </a>
                                     </figure>
@@ -146,7 +148,7 @@
                                 <div class="category category-ellipse">
                                     <figure class="category-media">
                                         <a href="shop-banner-sidebar.html">
-                                            <img src="assets/images/categories/category-20.jpg" alt="Categroy"
+                                            <img src="{{ asset('front-style/assets/images/categories/category-20.jp') }}g" alt="Categroy"
                                                 width="190" height="190" style="background-color: #E4E4E4;" />
                                         </a>
                                     </figure>
@@ -161,7 +163,7 @@
                                 <div class="category category-ellipse">
                                     <figure class="category-media">
                                         <a href="shop-banner-sidebar.html">
-                                            <img src="assets/images/categories/category-21.jpg" alt="Categroy"
+                                            <img src="{{ asset('front-style/assets/images/categories/category-21.jp') }}g" alt="Categroy"
                                                 width="190" height="190" style="background-color: #D3D8DE;" />
                                         </a>
                                     </figure>
@@ -176,7 +178,6 @@
                         <div class="swiper-pagination"></div>
                     </div>
                 </div>
-                <div class="container-fluid">
                     <!-- Start of Shop Category -->
                     <!-- Start of Shop Content -->
                     <div class="shop-content">
