95b4068e650964481e873b2dc5cf7fb244e18107
MohammadALSalamat <alomda.alslmat@gmail.com>
2022-05-17 15:35:21 +0400

    --- Auto-Git Commit ---

--- resources/views/mails/verify_vendors.blade.php
+++ resources/views/mails/verify_vendors.blade.php
@@ -1,205 +1,160 @@
-<!DOCTYPE html>
-<html lang="en">
+
+
 <head>
-<title>Recived An Email</title>
-
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width">
-<style type="text/css">
-    /* CLIENT-SPECIFIC STYLES */
-    #outlook a{padding:0;} /* Force Outlook to provide a "view in browser" message */
-    .ReadMsgBody{width:100%;} .ExternalClass{width:100%;} /* Force Hotmail to display emails at full width */
-    .ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {line-height: 100%;} /* Force Hotmail to display normal line spacing */
-    body, table, td, a{-webkit-text-size-adjust:100%; -ms-text-size-adjust:100%;} /* Prevent WebKit and Windows mobile changing default text sizes */
-    table, td{mso-table-lspace:0pt; mso-table-rspace:0pt;} /* Remove spacing between tables in Outlook 2007 and up */
-    img{-ms-interpolation-mode:bicubic;} /* Allow smoother rendering of resized image in Internet Explorer */
-
-    /* RESET STYLES */
-    body{margin:0; padding:0;}
-    img{border:0; height:auto; line-height:100%; outline:none; text-decoration:none;}
-    table{border-collapse:collapse !important;}
-    body{height:100% !important; margin:0; padding:0; width:100% !important;}
-
-    /* iOS BLUE LINKS */
-    .appleBody a {color:#68440a; text-decoration: none;}
-    .appleFooter a {color:#999999; text-decoration: none;}
-
-    /* MOBILE STYLES */
-    @media screen and (max-width: 525px) {
-
-        /* ALLOWS FOR FLUID TABLES */
-        table[class="wrapper"]{
-          width:100% !important;
+    <title></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <style type="text/css">
+        body,
+        table,
+        td,
+        a {
+            -webkit-text-size-adjust: 100%;
+            -ms-text-size-adjust: 100%;
         }
-
-        /* ADJUSTS LAYOUT OF LOGO IMAGE */
-        td[class="logo"]{
-          text-align: left;
-          padding: 20px 0 20px 0 !important;
+        
+        table,
+        td {
+            mso-table-lspace: 0pt;
+            mso-table-rspace: 0pt;
         }
-
-        td[class="logo"] img{
-          margin:0 auto!important;
+        
+        img {
+            -ms-interpolation-mode: bicubic;
         }
-
-        /* USE THESE CLASSES TO HIDE CONTENT ON MOBILE */
-        td[class="mobile-hide"]{
-          display:none;}
-
-        img[class="mobile-hide"]{
-          display: none !important;
+        
+        img {
+            border: 0;
+            height: auto;
+            line-height: 100%;
+            outline: none;
+            text-decoration: none;
         }
-
-        img[class="img-max"]{
-          max-width: 100% !important;
-          height:auto !important;
+        
+        table {
+            border-collapse: collapse !important;
         }
-
-        /* FULL-WIDTH TABLES */
-        table[class="responsive-table"]{
-          width:100%!important;
+        
+        body {
+            height: 100% !important;
+            margin: 0 !important;
+            padding: 0 !important;
+            width: 100% !important;
         }
-
-        /* UTILITY CLASSES FOR ADJUSTING PADDING ON MOBILE */
-        td[class="padding"]{
-          padding: 10px 5% 15px 5% !important;
+        
+        a[x-apple-data-detectors] {
+            color: inherit !important;
+            text-decoration: none !important;
+            font-size: inherit !important;
+            font-family: inherit !important;
+            font-weight: inherit !important;
+            line-height: inherit !important;
         }
-
-        td[class="padding-copy"]{
-          padding: 10px 5% 10px 5% !important;
-          text-align: center;
+        
+        @media screen and (max-width: 480px) {
+            .mobile-hide {
+                display: none !important;
+            }
+            .mobile-center {
+                text-align: center !important;
+            }
         }
-
-        td[class="padding-meta"]{
-          padding: 30px 5% 0px 5% !important;
-          text-align: center;
+        
+        div[style*="margin: 16px 0;"] {
+            margin: 0 !important;
         }
+    </style>
 
-        td[class="no-pad"]{
-          padding: 0 0 20px 0 !important;
-        }
-
-        td[class="no-padding"]{
-          padding: 0 !important;
-        }
-
-        td[class="section-padding"]{
-          padding: 50px 15px 50px 15px !important;
-        }
-
-        td[class="section-padding-bottom-image"]{
-          padding: 50px 15px 0 15px !important;
-        }
-
-        /* ADJUST BUTTONS ON MOBILE */
-        td[class="mobile-wrapper"]{
-            padding: 10px 5% 15px 5% !important;
-        }
-
-        table[class="mobile-button-container"]{
-            margin:0 auto;
-            width:100% !important;
-        }
-
-        a[class="mobile-button"]{
-            width:80% !important;
-            padding: 15px !important;
-            border: 0 !important;
-            font-size: 16px !important;
-        }
-
-    }
-</style>
-</head>
-<body style="margin: 0; padding: 0;">
+    <body style="margin: 0 !important; padding: 0 !important; background-color: #eeeeee;" bgcolor="#eeeeee">
 
 
-<!-- ONE COLUMN SECTION -->
-<table border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;">
-    <tr>
-        <td bgcolor="#ffffff" align="center" style="padding: 70px 15px 70px 15px;" class="section-padding">
-            <table border="0" cellpadding="0" cellspacing="0" width="500" class="responsive-table">
-                <tr>
-                    <td>
-                        <table width="100%" border="0" cellspacing="0" cellpadding="0">
-                            <tr>
-                                <td>
-                                    <!-- HERO IMAGE -->
-                                    <table width="100%" border="0" cellspacing="0" cellpadding="0">
-                                        <tbody>
-                                             <tr>
-                                                  <td class="padding-copy">
-                                                      <table width="100%" border="0" cellspacing="0" cellpadding="0">
-                                                          <tr>
-                                                              <td style="margin:0 auto">
-                                                                  <a href="https://9yards.ae" style="margin:auto" target="_blank"><img src="https://9yards.ae/storage/1/itajer_logo.png" width="100px" height="100px" border="0" alt="Can an email really be responsive?" style="display: block; padding: 0; color: #666666; text-decoration: none; font-family: Helvetica, arial, sans-serif; font-size: 16px; width: 200px; height: 200px; margin:auto" class="img-max"></a>
-                                                              </td>
-                                                            </tr>
-                                                        </table>
-                                                  </td>
-                                              </tr>
-                                        </tbody>
-                                    </table>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <!-- COPY -->
-                                    <table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <div style="display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: Open Sans, Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;">
+            For what reason would it be advisable for me to think about business content? That might be little bit risky to have crew member like them.
+        </div>
+
+        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+            <tr>
+                <td align="center" style="background-color: #eeeeee;" bgcolor="#eeeeee">
+
+                    <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:600px;">
+                        <tr>
+                            <td align="center" valign="top" style="font-size:0; padding: 35px;" bgcolor="#F44336">
+
+                                <div style="display:inline-block; max-width:50%; min-width:100px; vertical-align:top; width:100%;">
+                                    <table align="left" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:300px;">
                                         <tr>
-                                            <td align="center" style="font-size: 25px; font-family: Helvetica, Arial, sans-serif; color: #333333; padding-top: 30px;" class="padding-copy">Dear  {{  $newdata['name']  }}</td>
-                                        </tr>
-                                        <tr>
-                                        <td align="center" style="padding: 20px 0 0 0; font-size: 16px; line-height: 25px; font-family: Helvetica, Arial, sans-serif; color: #666666;" class="padding-copy">Thank you For your registered on ITajer website, <br> Our Team will view your request and updates you ASAP <br> If you have question you can Call us :0501413429 <br> Or Send us an email on : alomda.alslmat@gmail.com </td>
-                                        
+                                            <td align="left" valign="top" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 36px; font-weight: 800; line-height: 48px;" class="mobile-center">
+                                                <h1 style="font-size: 36px; font-weight: 800; margin: 0; color: #ffffff;">ITAJER SHOP</h1>
+                                            </td>
                                         </tr>
                                     </table>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <!-- BULLETPROOF BUTTON -->
-                                    <table width="100%" border="0" cellspacing="0" cellpadding="0" class="mobile-button-container">
+                                </div>
+
+                                <div style="display:inline-block; max-width:50%; min-width:100px; vertical-align:top; width:100%;" class="mobile-hide">
+                                    <table align="left" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:300px;">
                                         <tr>
-                                            <td align="center" style="padding: 25px 0 0 0;" class="padding-copy">
-                                                <table border="0" cellspacing="0" cellpadding="0" class="responsive-table">
+                                            <td align="right" valign="top" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; line-height: 48px;">
+                                                <table cellspacing="0" cellpadding="0" border="0" align="right">
                                                     <tr>
-                                                        <td align="center"><a href="http://9yards.ae" target="_blank" style="font-size: 16px; font-family: Helvetica, Arial, sans-serif; font-weight: normal; color: #ffffff; text-decoration: none; background-color: #5D9CEC; border-top: 15px solid #5D9CEC; border-bottom: 15px solid #5D9CEC; border-left: 25px solid #5D9CEC; border-right: 25px solid #5D9CEC; border-radius: 3px; -webkit-border-radius: 3px; -moz-border-radius: 3px; display: inline-block;" class="mobile-button">Back To Website &rarr;</a></td>
+                                                        <td style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400;">
+                                                            <p style="font-size: 18px; font-weight: 400; margin: 0; color: #ffffff;"><a href="#" target="_blank" style="color: #ffffff; text-decoration: none;">Shop &nbsp;</a></p>
+                                                        </td>
+                                                        <td style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 24px;">
+                                                            <a href="#" target="_blank" style="color: #ffffff; text-decoration: none;"><img src="https://img.icons8.com/color/48/000000/small-business.png" width="27" height="23" style="display: block; border: 0px;" /></a>
+                                                        </td>
                                                     </tr>
                                                 </table>
                                             </td>
                                         </tr>
                                     </table>
-                                </td>
-                            </tr>
-                        </table>
-                    </td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-</table>
-<!-- FOOTER -->
-<table border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;">
-    <tr>
-        <td bgcolor="#ffffff" align="center">
-            <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center">
-                <tr>
-                    <td style="padding: 20px 0px 20px 0px;">
-                        <!-- UNSUBSCRIBE COPY -->
-                        <table width="500" border="0" cellspacing="0" cellpadding="0" align="center" class="responsive-table">
-                            <tr>
-                                <td align="center" valign="middle" style="font-size: 12px; line-height: 18px; font-family: Helvetica, Arial, sans-serif; color:#666666;">
-                                    <span class="appleFooter" style="color:#666666;">Breakwater, Beside Abu Dhabi Marine Sports Club - Breakwater Rd - Abu Dhabi</span><br><a class="original-only" style="color: #666666; text-decoration: none;"></a><span class="original-only" style="font-family: Arial, sans-serif; font-size: 12px; color: #444444;">&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;</span><a style="color: #666666; text-decoration: none;">9Yards Team 2017/2021</a>
-                                </td>
-                            </tr>
-                        </table>
-                    </td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-</table>
+                                </div>
 
-</body>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="center" style="padding: 35px 35px 20px 35px; background-color: #ffffff;" bgcolor="#ffffff">
+                                <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:600px;">
+                                    <tr>
+                                        <td align="center" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding-top: 25px;">
+                                            <img src="https://img.icons8.com/carbon-copy/100/000000/checked-checkbox.png" width="125" height="120" style="display: block; border: 0px;" /><br>
+                                            <h2 style="font-size: 30px; font-weight: 800; line-height: 36px; color: #333333; margin: 0;">
+                                               Dear {{  $newdata['name']  }} !
+                                            </h2>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td align="left" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding-top: 10px;">
+                                            <p style="font-size: 16px; font-weight: 400; line-height: 24px; color: #777777;">
+                                                Thank you For your registered on ITajer website, <br> Our Team will view your request and updates you ASAP <br> If you have question you can Call us :0501413429 <br> Or Send us an email on : alomda.alslmat@gmail.com
+
+                                            </p>
+                                        </td>
+                                    </tr>
+                                </table>
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="center" style="padding: 35px; background-color: #ffffff;" bgcolor="#ffffff">
+                                <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:600px;">
+                                    <td align="left" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 24px;">
+                                        <p style="font-size: 14px; font-weight: 400; line-height: 20px; color: #777777;">
+                                            Please Note:: This email was sent from a notification-only So Please Do not reply back to this email. <a href="#" target="_blank" style="color: #777777;">unsusbscribe</a>.
+                                        </p>
+                                    </td>
+                        </tr>
+                        </table>
+                        </td>
+            </tr>
+            </table>
+            </td>
+            </tr>
+        </table>
+
+    </body>
+
 </html>
