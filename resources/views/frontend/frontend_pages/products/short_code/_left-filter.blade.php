f783fbcd5cc87e9761adeedc72b998706cb250fe
MohammadALSalamat <alomda.alslmat@gmail.com>
2022-05-18 09:45:33 +0400

    --- Auto-Git Commit ---

--- resources/views/frontend/frontend_pages/products/short_code/_left-filter.blade.php
+++ resources/views/frontend/frontend_pages/products/short_code/_left-filter.blade.php
@@ -1,5 +1,7 @@
 <form action="{{ route('shop_filter') }}" method="POST">
 @csrf
+
+@if(Config::get('app.locale') == 'en')
 <!-- Start of Collapsible widget -->
 <div class="widget widget-collapsible">
      <h3 class="widget-title"><label>All Categories</label></h3>
@@ -30,6 +32,44 @@
 </div>
 <!-- End of Collapsible Widget -->
 
+@else
+<!-- Start of Collapsible widget -->
+<div class="widget widget-collapsible">
+    <h3 class="widget-title"><label>كل اﻷقسام</label></h3>
+    <ul class="widget-body filter-items search-ul">
+        <!-- get the category name and check the box if exists -->
+        @if (!empty($_GET['category']))
+        @php
+        $filter_cats = explode(',',$_GET['category']);
+        @endphp
+        @endif
+        <!-- get the category name and check the box if exists -->
+        @if (count($main_categories) > 0)
+            @foreach ($main_categories as $single_cat)
+            <div class="d-flex mb-2 items-center" style="align-items: center;">
+                <input type="checkbox" @if (!empty($filter_cats) && in_array($single_cat->slug,$filter_cats))
+                checked
+                @endif name="category[]" value="{{ $single_cat->slug }}" id="{{ $single_cat->slug }}"
+                onchange="this.form.submit();" >
+                <label class="pl-3" for="{{ $single_cat->slug }}"> {{ $single_cat->ar_title }}
+                    ({{ count($single_cat->one_cat_has_many_products) }})</label>
+            </div>
+            @endforeach
+        @else
+        <li class=" text-red-700"> لا يوجد اقسام اﻷن</li>
+        @endif
+
+    </ul>
+</div>
+<!-- End of Collapsible Widget -->
+
+@endif
+
+
+
+
+@if(Config::get('app.locale') == 'en')
+
 <div class="widget widget-collapsible">
     <h3 class="widget-title"><label>All Brands</label></h3>
     <ul class="widget-body filter-items search-ul">
@@ -57,6 +97,39 @@
 
     </ul>
 </div>
+
+@else
+
+<div class="widget widget-collapsible">
+    <h3 class="widget-title"><label>All Brands</label></h3>
+    <ul class="widget-body filter-items search-ul">
+        <!-- get the category name and check the box if exists -->
+        @if (!empty($_GET['brand']))
+        @php
+        $filter_brands_rel_product = explode(',',$_GET['brand']);
+        @endphp
+        @endif
+        <!-- get the category name and check the box if exists -->
+        @if (count($brands_rel_product) > 0)
+            @foreach ($brands_rel_product as $single_cat)
+            <div class="d-flex mb-2 items-center" style="align-items: center;">
+                <input type="checkbox" @if (!empty($filter_brands_rel_product) && in_array($single_cat->slug,$filter_brands_rel_product))
+                checked
+                @endif name="brand[]" value="{{ $single_cat->slug }}" id="{{ $single_cat->slug }}"
+                onchange="this.form.submit();" >
+                <label class="pl-3" for="{{ $single_cat->slug }}"> {{ $single_cat->ar_title }}
+                    ({{ count($single_cat->products) }})</label>
+            </div>
+            @endforeach
+        @else
+        <li class=" text-red-700"> There is no Categries to Filter</li>
+        @endif
+
+    </ul>
+</div>
+
+@endif
+
 <!-- Start of Collapsible Vendors -->
 <div class="widget widget-collapsible">
      <h3 class="widget-title"><label>Vendors</label></h3>
