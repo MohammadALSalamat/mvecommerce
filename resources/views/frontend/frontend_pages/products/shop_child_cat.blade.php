47d10f55e1878d4ea95d2a2b663a786f5e153821
MohammadALSalamat <alomda.alslmat@gmail.com>
2022-05-18 13:54:22 +0400

    --- Auto-Git Commit ---

--- resources/views/frontend/frontend_pages/products/shop_child_cat.blade.php
+++ resources/views/frontend/frontend_pages/products/shop_child_cat.blade.php
@@ -165,12 +165,22 @@
                                     </div>
                                 </figure>
                                 <div class="product-details">
+                                    @if(Config::get('app.locale') == 'en')
+
                                     <div class="product-cat">
                                         <a href="{{ route('shop_special_category',$category_product->slug) }}">{{ $category_product->title }}</a>
                                     </div>
                                     <h4 class="product-name">
                                         <a href="{{ route('singleproduct',$products_category->slug) }}">{{ $products_category->title }}</a>
                                     </h4>
+                                    @else 
+                                    <div class="product-cat">
+                                        <a href="{{ route('shop_special_category',$category_product->slug) }}">{{ $category_product->ar_title }}</a>
+                                    </div>
+                                    <h4 class="product-name">
+                                        <a href="{{ route('singleproduct',$products_category->slug) }}">{{ $products_category->ar_title }}</a>
+                                    </h4>
+                                    @endif
                                     <div class="ratings-container">
                                         <div class="ratings-full">
                                             <span class="ratings" 
@@ -220,105 +230,7 @@
                     <h1 class="text-center" style="color: #fff;background:red;padding:20pxborder:1px solid red"> There is No Products related to this category</h1>
                 @endif
                 </div>
-                <!-- End of Shop Main Content -->
-
-                <!-- Start of Sidebar, Shop Sidebar -->
-                <aside class="sidebar shop-sidebar left-sidebar sticky-sidebar-wrapper">
-                    <!-- Start of Sidebar Overlay -->
-                    <div class="sidebar-overlay"></div>
-                    <a class="sidebar-close" href="#"><i class="close-icon"></i></a>
-                    <!-- Start of Sidebar Content -->
-                    <div class="sidebar-content scrollable">
-                        <div class="filter-actions">
-                            <label>Filter :</label>
-                            <a href="#" class="btn btn-dark btn-link filter-clean">Clean All</a>
-                        </div>
-                        <!-- Start of Collapsible widget -->
-                        <div class="widget widget-collapsible">
-                            <h3 class="widget-title"><span>All Categories</span></h3>
-                            <ul class="widget-body filter-items search-ul">
-                                <li><a href="#">Accessories</a></li>
-                                <li><a href="#">Babies</a></li>
-                                <li><a href="#">Beauty</a></li>
-                                <li><a href="#">Decoration</a></li>
-                                <li><a href="#">Electronics</a></li>
-                                <li><a href="#">Fashion</a></li>
-                                <li><a href="#">Food</a></li>
-                                <li><a href="#">Furniture</a></li>
-                                <li><a href="#">Kitchen</a></li>
-                                <li><a href="#">Medical</a></li>
-                                <li><a href="#">Sports</a></li>
-                                <li><a href="#">Watches</a></li>
-                            </ul>
-                        </div>
-                        <!-- End of Collapsible Widget -->
-
-                        <!-- Start of Collapsible Widget -->
-                        <div class="widget widget-collapsible">
-                            <h3 class="widget-title"><span>Price</span></h3>
-                            <div class="widget-body">
-                                <ul class="filter-items search-ul">
-                                    <li><a href="#">$0.00 - $100.00</a></li>
-                                    <li><a href="#">$100.00 - $200.00</a></li>
-                                    <li><a href="#">$200.00 - $300.00</a></li>
-                                    <li><a href="#">$300.00 - $500.00</a></li>
-                                    <li><a href="#">$500.00+</a></li>
-                                </ul>
-                                <form class="price-range">
-                                    <input type="number" name="min_price" class="min_price text-center"
-                                        placeholder="$min"><span class="delimiter">-</span><input type="number"
-                                        name="max_price" class="max_price text-center" placeholder="$max"><a
-                                        href="#" class="btn btn-primary btn-rounded">Go</a>
-                                </form>
-                            </div>
-                        </div>
-                        <!-- End of Collapsible Widget -->
-
-                        <!-- Start of Collapsible Widget -->
-                        <div class="widget widget-collapsible">
-                            <h3 class="widget-title"><span>Size</span></h3>
-                            <ul class="widget-body filter-items item-check mt-1">
-                                <li><a href="#">Extra Large</a></li>
-                                <li><a href="#">Large</a></li>
-                                <li><a href="#">Medium</a></li>
-                                <li><a href="#">Small</a></li>
-                            </ul>
-                        </div>
-                        <!-- End of Collapsible Widget -->
-
-                        <!-- Start of Collapsible Widget -->
-                        <div class="widget widget-collapsible">
-                            <h3 class="widget-title"><span>Brand</span></h3>
-                            <ul class="widget-body filter-items item-check mt-1">
-                                <li><a href="#">Elegant Auto Group</a></li>
-                                <li><a href="#">Green Grass</a></li>
-                                <li><a href="#">Node Js</a></li>
-                                <li><a href="#">NS8</a></li>
-                                <li><a href="#">Red</a></li>
-                                <li><a href="#">Skysuite Tech</a></li>
-                                <li><a href="#">Sterling</a></li>
-                            </ul>
-                        </div>
-                        <!-- End of Collapsible Widget -->
-
-                        <!-- Start of Collapsible Widget -->
-                        <div class="widget widget-collapsible">
-                            <h3 class="widget-title"><span>Color</span></h3>
-                            <ul class="widget-body filter-items item-check">
-                                <li><a href="#">Black</a></li>
-                                <li><a href="#">Blue</a></li>
-                                <li><a href="#">Brown</a></li>
-                                <li><a href="#">Green</a></li>
-                                <li><a href="#">Grey</a></li>
-                                <li><a href="#">Orange</a></li>
-                                <li><a href="#">Yellow</a></li>
-                            </ul>
-                        </div>
-                        <!-- End of Collapsible Widget -->
-                    </div>
-                    <!-- End of Sidebar Content -->
-                </aside>
-                <!-- End of Shop Sidebar -->
+               
             </div>
             <!-- End of Shop Content -->
         </div>
