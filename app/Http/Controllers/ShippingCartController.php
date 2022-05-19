eff8653053e00ce82545be44c861380e3c2f7661
MohammadALSalamat <alomda.alslmat@gmail.com>
2022-05-19 15:47:38 +0400

    --- Auto Git Commit ---

--- app/Http/Controllers/ShippingCartController.php
+++ app/Http/Controllers/ShippingCartController.php
@@ -22,6 +22,7 @@ class ShippingCartController extends Controller
 
         // this has a model in the product model
         $product = product::getProductByCart($product_id);
+        dd($product);
 
         if(!empty($product[0]['offer_price']) || $product[0]['offer_price'] != null){
             $price = $product[0]['offer_price'];
