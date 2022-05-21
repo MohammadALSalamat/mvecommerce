da46490ed7ff73ab5ea5c743797f5caaf6171d51
MohammadALSalamat <alomda.alslmat@gmail.com>
2022-05-21 17:25:42 +0400

    --- Auto Git Commit ---

--- resources/views/Seller/seller_pages/dashboard.blade.php
+++ resources/views/Seller/seller_pages/dashboard.blade.php
@@ -7,7 +7,7 @@
     <div class="content-body">
       <!-- eCommerce statistic -->
       <div class="row">
-        <div class="col-xl-3 col-lg-6 col-12">
+        <div class="col-xl-4 col-lg-6 col-12">
           <div class="card pull-up">
             <div class="card-content">
               <div class="card-body">
@@ -33,7 +33,7 @@
             </div>
           </div>
         </div>
-        <div class="col-xl-3 col-lg-6 col-12">
+        <div class="col-xl-4 col-lg-6 col-12">
           <div class="card pull-up">
             <div class="card-content">
               <div class="card-body">
@@ -63,34 +63,7 @@
             </div>
           </div>
         </div>
-        <div class="col-xl-3 col-lg-6 col-12">
-          <div class="card pull-up">
-            <div class="card-content">
-              <div class="card-body">
-                <div class="media d-flex">
-                  <div class="media-body text-left">
-                    @if (Config::get('app.locale') == 'en')
-                    <h3 class="success">{{ $countSoldProduct }}</h3>
-                    <h6>Sold Product</h6>
-                    @else
-                    <h3 class="success">{{ $countSoldProduct }}</h3>
-                    <h6>التجار المنتظرين</h6>
-                    @endif
-                  </div>
-                  <div>
-                    <i class="icon-user-follow success font-large-2 float-right"></i>
-                  </div>
-                </div>
-                <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-                  <div class="progress-bar bg-gradient-x-success" role="progressbar" style="width: 75%"
-                    aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-xl-3 col-lg-6 col-12">
+        <div class="col-xl-4 col-lg-6 col-12">
           <div class="card pull-up">
             <div class="card-content">
               <div class="card-body">
