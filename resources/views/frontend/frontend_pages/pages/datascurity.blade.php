6558265b006803c4fed65b5c4f70177b7a924e6e
MohammadALSalamat <alomda.alslmat@gmail.com>
2022-06-09 16:10:08 +0400

    --- Auto Git Commit ---

---
+++ resources/views/frontend/frontend_pages/pages/datascurity.blade.php
@@ -0,0 +1,42 @@
+@extends('frontend.frontend_layout.main_desgin')
+@section('mytitle','Data Security')
+
+@section('content')
+
+<main class="main">
+    <!-- Start of Page Header -->
+    <div class="page-header" style="background:url('/storage/photos/1/Artboard 1 copy-100.jpg');background-size:cover">
+        <div class="container">
+            <h1 class="page-title mb-0 text-white">Data Security</h1>
+        </div>
+    </div>
+    <!-- End of Page Header -->
+
+    <!-- Start of Page Content -->
+    <div class="page-content">
+        <div class="container">
+            <section class="boost-section pt-10 pb-10">
+                <div class="container mt-10 mb-9">
+                    <div class="row align-items-center mb-10">
+                        <div class="col-md-6 pl-lg-8 mb-8">
+                            <h4 class="title text-left">Data Security</h4>
+                            <p class="mb-6">
+                                Essentially will use its best efforts to protect your privacy. Authorized employees
+                                within the company on a need-to-know basis only use any information collected from
+                                individual customers. We will not sell, share, or rent your personal information to any
+                                third party or use your e-mail address for unsolicited mail. Any emails sent by the
+                                Company will only be in connection with the provision of agreed services and products.
+                                We will use our best efforts to provide the highest level of support in order to
+                                maintain maximum information security. However, as you are aware – we cannot guarantee
+                                absolute security for the website and therefore cannot be held liable against it in any
+                                manner whatsoever.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </div>
+    </div>
+</main>
+@endsection
