f49b38375f0823671c940b5bf39f1f91898f66d4
MohammadALSalamat <alomda.alslmat@gmail.com>
2022-06-09 15:43:16 +0400

    --- Auto Git Commit ---

---
+++ resources/views/frontend/frontend_pages/pages/privacy_policy.blade.php
@@ -0,0 +1,79 @@
+@extends('frontend.frontend_layout.main_desgin')
+@section('mytitle','Privacy Policy')
+
+@section('content')
+
+<main class="main">
+    <!-- Start of Page Header -->
+    <div class="page-header" style="background:url('/storage/photos/1/Artboard 1 copy-100.jpg');background-size:cover">
+        <div class="container">
+            <h1 class="page-title mb-0 text-white">Privacy Policy</h1>
+        </div>
+    </div>
+    <!-- End of Page Header -->
+
+    <!-- Start of Page Content -->
+    <div class="page-content">
+        <div class="container">
+            <section class="boost-section pt-10 pb-10">
+                <div class="container mt-10 mb-9">
+                    <div class="row align-items-center mb-10">
+                        <div class="col-md-6 pl-lg-8 mb-8">
+                            <h4 class="title text-left">Privacy Policy</h4>
+                            <p class="mb-6">
+                                Your privacy is of the utmost importance to us. Your information will never be sold to
+                                any outside party. We collect aggregate data on what pages consumers’ access or visit,
+                                user-specific information from the pages consumers’ access or visit, information
+                                volunteered by the consumer, such as survey information and/or site registrations, and
+                                name, address, and telephone number.<br>
+                                The information we collect is used to improve the content of our web page, customize the
+                                content and/or layout of our page, notify consumers about updates to our website, track
+                                consumer preferences, and enhance the consumer experience. We may also use this
+                                information for marketing and promotional purposes, as well as statistical analysis.
+                            </p>
+                            <p>
+                                We will treat your personal information as strictly private and confidential. The
+                                information so provided by you to the Company or captured by the Company may be used for
+                                a number of purposes connected with the Company’s business operations which may include
+                                the following:
+                                Processing orders or applications
+                                Provisioning of services, improvement of services, recommending various products or
+                                services including those of third parties
+                                Dealing with requests, enquiries and complaints and customer related activities
+                                Marketing products and services and its analysis
+                            </p>
+                            <p>
+                                Disclose the information for abiding with laws and law enforcement / regulatory requests
+                                The Company may provide your information or data to its partners, associates, service
+                                providers or other third parties to provide, advertise or market their legitimate
+                                products and/or services which may be of your interest. You will have the choice to ‘opt
+                                out’ of such marketing or promotional communications at your will.
+                                Your consent being part of the terms and conditions through which the Company provide
+                                you with a service. Your consent may be implicit or implied or through course of
+                                conduct.
+                                To enforce our terms and conditions
+                            </p>
+                            <p>
+                                To protect our rights, privacy, safety or property, and/or that of our affiliates, you
+                                or others.
+                                To safeguard your information, we use third-party software to securely collect payments,
+                                maintain our customer contact information, and host our websites. All online
+                                transactions are encrypted. For the purpose of authorization and completion of
+                                transactions, information you submit when making an online purchase is shared with
+                                payment processors and your credit card company. We do not have access to any of your
+                                credit card information once you make a purchase.
+                                If you would like to: access, correct, amend or delete any personal information we have
+                                about you, register a complaint, or simply want more information, contact our Privacy
+                                Compliance Officer at <a href="mailto:info@itajer.ae"> info@itajer.ae </a>
+                            </p>
+                        </div>
+
+                    </div>
+
+                </div>
+            </section>
+
+        </div>
+    </div>
+</main>
+@endsection
---
+++ resources/views/frontend/frontend_pages/pages/ret&cancl.blade.php
@@ -0,0 +1,47 @@
+@extends('frontend.frontend_layout.main_desgin')
+@section('mytitle','Refund & cancellation Policy')
+
+@section('content')
+
+<main class="main">
+    <!-- Start of Page Header -->
+    <div class="page-header" style="background:url('/storage/photos/1/Artboard 1 copy-100.jpg');background-size:cover">
+        <div class="container">
+            <h1 class="page-title mb-0 text-white">Refund & cancellation Policy</h1>
+        </div>
+    </div>
+    <!-- End of Page Header -->
+
+    <!-- Start of Page Content -->
+    <div class="page-content">
+        <div class="container">
+            <section class="boost-section pt-10 pb-10">
+                <div class="container mt-10 mb-9">
+                    <div class="row align-items-center mb-10">
+                        <div class="col-md-6 pl-lg-8 mb-8">
+                            <h4 class="title text-left">Refund & cancellation</h4>
+                            <p class="mb-6">
+                                We do not accept returns for perishable goods & services. Please get in touch with our
+                                Customer Care Team if you have any concerns or questions about a specific item or
+                                service.
+                                If your order was damaged in transit, or if you've received the wrong order within 3
+                                working days from delivery time, please reach out to our customer service team
+                                immediately by calling us or sending email +971588052313 (or by info@itajer.ae) and we
+                                will send you a replacement item or provide another solution.
+                                In case you feel that your account has been charged in error, you must notify us within
+                                7 days of purchase.
+                                Any questions, complaints, or claims regarding the Application should be directed to:
+                                info@itajer.ae
+                                These inquiries will be handled on a case-by-case basis. 
+                            </p>
+                        </div>
+
+                    </div>
+
+                </div>
+            </section>
+
+        </div>
+    </div>
+</main>
+@endsection
