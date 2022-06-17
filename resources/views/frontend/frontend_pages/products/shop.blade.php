c4fa92be8a4beb1271bcefe9abc0b2cd1fbd95f6
MohammadALSalamat <alomda.alslmat@gmail.com>
2022-06-17 15:50:55 +0400

    --- Auto Git Commit ---

--- resources/views/frontend/frontend_pages/products/shop.blade.php
+++ resources/views/frontend/frontend_pages/products/shop.blade.php
@@ -4,7 +4,6 @@
  <!-- Plugins CSS -->
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
-  
 @section('content')
         <!-- Start of Main -->
         <main class="main">
@@ -47,6 +46,23 @@
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         </button>
                     </div>
+                    @else
+
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="{{ asset('storage/photos/1/Artboard 10-1001.jpg') }}" class="d-block w-100" alt="slider">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            </button>
+        </div>
                     @endif
                 </section>
                 <div class="container">
