862b61393d80802139c6207370e859ce2101e8d1
MohammadALSalamat <alomda.alslmat@gmail.com>
2022-06-17 16:04:20 +0400

    --- Auto Git Commit ---

--- resources/views/frontend/frontend_pages/products/shop.blade.php
+++ resources/views/frontend/frontend_pages/products/shop.blade.php
@@ -48,21 +48,21 @@
                     </div>
                     @else
 
-        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="{{ asset('storage/photos/1/Artboard 10-1001.jpg') }}" class="d-block w-100" alt="slider">
+                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="{{ asset('storage/photos/1/Artboard 10-1001.jpg') }}" class="d-block w-100" alt="slider">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+                        data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+                        data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    </button>
                 </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
-                data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
-                data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            </button>
-        </div>
                     @endif
                 </section>
                 <div class="container">
