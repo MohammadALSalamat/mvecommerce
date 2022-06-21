09def2dcad6f12414a092fd65a24ee2492298a74
MohammadALSalamat <alomda.alslmat@gmail.com>
2022-06-21 22:11:04 +0400

    --- Auto Git Commit ---

--- app/Http/Controllers/HelpUsController.php
+++ app/Http/Controllers/HelpUsController.php
@@ -38,11 +38,28 @@ public function helpus()
         $data = $request->all();
         $current_user = User::where('id',$data['user_id'])->first();
         $current_post = helpUs::where('id',$data['post_id'])->first();
+        $totalLikes = likeDislike::get();
+        
         dd($current_post);
+
+        $liketotal = array();
+
        if($data['post_type'] == 'like'){
         $check_user_if_checked_thumbs = likeDislike::where('user_id',$current_user->id)->where('post_id',$current_post->id)->count();
         if($check_user_if_checked_thumbs > 0){
-
+            $likes = likeDislike::where('post_id',$current_post->id)->get();
+            foreach($likes as $like){
+                array_push($liketotal,$like->like);
+            }
+            $result = array_sum($liketotal) - 1;
+            likeDislike::where('user_id',$current_user->id)->where('post_id',$current_post->id)->update([
+                'like' => $result ,
+            ]);
+        }else{
+            $add_newlike = new likeDislike();
+            $add_newlike->user_id = $current_user->id;
+            $add_newlike->post_id = $current_post->id;
+            $add_newlike->like = ;
         }
 
        }else{
