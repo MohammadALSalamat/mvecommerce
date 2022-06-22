ed6cf9bd5a4f1f4e5e022319864922e82c96ae2b
MohammadALSalamat <alomda.alslmat@gmail.com>
2022-06-22 09:19:16 +0400

    --- Auto Git Commit ---

--- resources/views/frontend/frontend_pages/pages/helpUs.blade.php
+++ resources/views/frontend/frontend_pages/pages/helpUs.blade.php
@@ -299,7 +299,7 @@
                                             <a class="like" href="javascript:valid(0)" data-type="like" data-userid={{ $current_user->id }} data-postid="{{ $post->id }}"  style="margin-right:10px;padding:10px 25px;text-align:center"> <i class="fa fa-thumbs-up"></i>
                                                 <div class="text"><span class="likescount" data-id = "likescount{{ $post->id }}">{{ \App\Models\likeDislike::where('help_us_id',$post->id)->count() }}</span></div>
                                             </a>
-                                            <a href="javascript:valid(0)" class="like" data-type="like" data-userid={{ $current_user->id }} data-postid="{{ $post->id }}" data-type="dislike" style="padding:10px 25px;text-align:center"> <i class="fa fa-thumbs-down"></i>
+                                            <a href="javascript:valid(0)" class="like" data-userid={{ $current_user->id }} data-postid="{{ $post->id }}" data-type="dislike" style="padding:10px 25px;text-align:center"> <i class="fa fa-thumbs-down"></i>
                                                 <div class="text"><span class="dislikecount">{{ \App\Models\likeDislike::where('help_us_id',$post->id)->count() }}</span></div>
                                             </a>
                                             </div>
