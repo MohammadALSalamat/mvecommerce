e2c3741ce1d7195709d5c62a9a8965cfc08c8521
MohammadALSalamat <alomda.alslmat@gmail.com>
2022-06-25 18:56:17 +0400

    --- Auto Git Commit ---

--- app/Models/delivery.php
+++ app/Models/delivery.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class delivery extends Model
+class delivery extends Authenticatable
+
 {
     use HasFactory;
+    protected $guard = 'deliveries';
+
 }
