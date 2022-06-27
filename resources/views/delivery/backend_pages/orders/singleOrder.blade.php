0b8e8d5c13bd77ed687576ad6347f8a40d598a33
MohammadALSalamat <alomda.alslmat@gmail.com>
2022-06-27 09:43:56 +0400

    --- Auto Git Commit ---

--- resources/views/delivery/backend_pages/orders/singleOrder.blade.php
+++ resources/views/delivery/backend_pages/orders/singleOrder.blade.php
@@ -15,7 +15,9 @@
 @section('content')
 
 <div class="app-content content">
+  
   <div class="content-wrapper">
+    
     <div class="content-header row">
       <div class="mb-2 content-header-left col-md-6 col-12 breadcrumb-new">
         <h3 class="mb-0 content-header-title d-inline-block">Advanced DataTable</h3>
@@ -40,7 +42,170 @@
       </div>
     </div>
     <div class="content-body">
-
+      <section id="basic-form-layouts">
+        <div class="row match-height">
+          <div class="col-md-6">
+            <div class="card">
+              <div class="card-header">
+                <h4 class="card-title" id="basic-layout-form">Order Details</h4>
+                <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+                <div class="heading-elements">
+                  <ul class="list-inline mb-0">
+                    <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                    <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
+                    <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                    <li><a data-action="close"><i class="ft-x"></i></a></li>
+                  </ul>
+                </div>
+              </div>
+              <div class="card-content collapse show">
+                <div class="card-body">
+                  <form class="form">
+                    <div class="form-body">
+                      <h4 class="form-section"><i class="la la-paperclip"></i> Requirements</h4>
+                      <div class="row">
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <label for="projectinput5">Interested in</label>
+                            <select id="projectinput5" name="interested" class="form-control">
+                              <option value="none" selected="" disabled="">Interested in</option>
+                              <option value="design">design</option>
+                              <option value="development">development</option>
+                              <option value="illustration">illustration</option>
+                              <option value="branding">branding</option>
+                              <option value="video">video</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <label for="projectinput6">Budget</label>
+                            <select id="projectinput6" name="budget" class="form-control">
+                              <option value="0" selected="" disabled="">Budget</option>
+                              <option value="less than 5000$">less than 5000$</option>
+                              <option value="5000$ - 10000$">5000$ - 10000$</option>
+                              <option value="10000$ - 20000$">10000$ - 20000$</option>
+                              <option value="more than 20000$">more than 20000$</option>
+                            </select>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="form-actions">
+                      <button type="submit" class="btn btn-primary">
+                        <i class="la la-check-square-o"></i> Change Status
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="card">
+              <div class="card-header">
+                <h4 class="card-title" id="basic-layout-colored-form-control">User Info</h4>
+                <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+                <div class="heading-elements">
+                  <ul class="list-inline mb-0">
+                    <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                    <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
+                    <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                    <li><a data-action="close"><i class="ft-x"></i></a></li>
+                  </ul>
+                </div>
+              </div>
+              <div class="card-content collapse show">
+                <div class="card-body">
+                 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="basic-form-layouts">
+        <div class="row match-height">
+          <div class="col-md-6">
+            <div class="card">
+              <div class="card-header">
+                <h4 class="card-title" id="basic-layout-form">Order Details</h4>
+                <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+                <div class="heading-elements">
+                  <ul class="list-inline mb-0">
+                    <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                    <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
+                    <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                    <li><a data-action="close"><i class="ft-x"></i></a></li>
+                  </ul>
+                </div>
+              </div>
+              <div class="card-content collapse show">
+                <div class="card-body">
+                  <form class="form">
+                    <div class="form-body">
+                      <h4 class="form-section"><i class="la la-paperclip"></i> Requirements</h4>
+                      <div class="row">
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <label for="projectinput5">Interested in</label>
+                            <select id="projectinput5" name="interested" class="form-control">
+                              <option value="none" selected="" disabled="">Interested in</option>
+                              <option value="design">design</option>
+                              <option value="development">development</option>
+                              <option value="illustration">illustration</option>
+                              <option value="branding">branding</option>
+                              <option value="video">video</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <label for="projectinput6">Budget</label>
+                            <select id="projectinput6" name="budget" class="form-control">
+                              <option value="0" selected="" disabled="">Budget</option>
+                              <option value="less than 5000$">less than 5000$</option>
+                              <option value="5000$ - 10000$">5000$ - 10000$</option>
+                              <option value="10000$ - 20000$">10000$ - 20000$</option>
+                              <option value="more than 20000$">more than 20000$</option>
+                            </select>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="form-actions">
+                      <button type="submit" class="btn btn-primary">
+                        <i class="la la-check-square-o"></i> Change Status
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="card">
+              <div class="card-header">
+                <h4 class="card-title" id="basic-layout-colored-form-control">User Info</h4>
+                <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+                <div class="heading-elements">
+                  <ul class="list-inline mb-0">
+                    <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                    <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
+                    <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                    <li><a data-action="close"><i class="ft-x"></i></a></li>
+                  </ul>
+                </div>
+              </div>
+              <div class="card-content collapse show">
+                <div class="card-body">
+                 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       <!-- File export table -->
       <section id="file-export">
         <div class="row">
