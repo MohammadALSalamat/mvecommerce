ff70e02ec309230abb988ba6b04de398fe4526f9
MohammadALSalamat <alomda.alslmat@gmail.com>
2022-06-27 15:21:38 +0400

    --- Auto Git Commit ---

--- app/Mail/orderVendorEmail.php
+++ app/Mail/orderVendorEmail.php
@@ -11,15 +11,15 @@ class orderVendorEmail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
-    private  $data;
+    private  $order_email_imfo;
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct($data)
+    public function __construct($order_email_imfo)
     {
-        $this->data = $data;
+        $this->order_email_imfo = $order_email_imfo;
     }
 
     /**
@@ -32,6 +32,6 @@ class orderVendorEmail extends Mailable implements ShouldQueue
         return $this->from('support@9yards.ae')
         ->subject('New Order your Product')
         ->view('mails.vendor_order_admin_email')
-        ->with('data', $this->data);
+        ->with('order_email_imfo', $this->order_email_imfo);
     }
 }
