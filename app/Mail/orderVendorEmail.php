ce2d09cd0ee5a7d481e162b8f2e5397196ac40de
MohammadALSalamat <alomda.alslmat@gmail.com>
2022-06-27 17:09:03 +0400

    --- Auto Git Commit ---

--- app/Mail/orderVendorEmail.php
+++ app/Mail/orderVendorEmail.php
@@ -32,6 +32,6 @@ class orderVendorEmail extends Mailable implements ShouldQueue
         return $this->from('support@9yards.ae')
         ->subject('New Order your Product')
         ->view('mails.vendor_order_admin_email')
-        ->with('order', $this->data_for_vendor);
+        ->with('data_for_vendor', $this->data_for_vendor);
     }
 }
