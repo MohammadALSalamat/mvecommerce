4e04b9489ad634cb3d31d03fd759278f1afc34a0
MohammadALSalamat <alomda.alslmat@gmail.com>
2022-07-10 15:37:20 +0400

    --- Auto Git Commit ---

--- app/Http/Controllers/SellerController.php
+++ app/Http/Controllers/SellerController.php
@@ -49,26 +49,35 @@ class SellerController extends Controller
     // admin dashboard
     public function dashboard(){
         $current_user = Seller::find(Auth::guard('seller')->user()->id);
+
         $notify_subscripe = subscription::where('seller_id',$current_user->id)->first();
+
         $message_notify = null;
         if(!empty($notify_subscripe) || $notify_subscripe != null){
-            if(Carbon::parse($notify_subscripe->ends_at)->diffInDays(Carbon::now()) < 10 ){
+            if(Carbon::parse($notify_subscripe->ends_at)->diffInDays(Carbon::now()) < 20 ){
             $message_notify ='your days left is less than 10 days please subscripe before /  '. $notify_subscripe->ends_at .'/ Just ' . Carbon::parse($notify_subscripe->ends_at)->diffInDays(Carbon::now()) . ' Days left Otherwise your account will not work';
             }
+
             if(Carbon::parse($notify_subscripe->ends_at)->diffInDays(Carbon::now()) <= 0 ){
                 Seller::where('id',$current_user->id)->update([
                     'is_verify'=> 0,
                 ]);
             }
         }
+
         $count_vendors = User::where('status',0)->count();
+
         $products_sold = Order::count();
+
         $Orders = Order::latest()->get(); // last 6 orders
+
         $order_product = product::orderBy('id','DESC')->with('orders')->where('vendor_id',$current_user->id)->where('added_by','seller')->take(6)->get();
+
         $Total_order_products =  product::orderBy('id','DESC')->with('orders')->where('vendor_id',$current_user->id)->where('added_by','seller')->get();
       
         $products = product::where('vendor_id',$current_user->id)->where('added_by','seller')->count();
         // seller product that got sold
+        
         $total = array(); // get the profit of seller 
         $sold_products = array();
         if ($Total_order_products->count() > 0) {
