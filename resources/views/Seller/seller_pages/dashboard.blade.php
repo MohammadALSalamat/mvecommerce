434b1560598ee3245c612fb0c7a251363b296a95
MohammadALSalamat <alomda.alslmat@gmail.com>
2022-07-21 19:48:56 +0400

    --- Auto Git Commit ---

--- resources/views/Seller/seller_pages/dashboard.blade.php
+++ resources/views/Seller/seller_pages/dashboard.blade.php
@@ -155,12 +155,7 @@
                 
                     <tr>
                       <td class="text-truncate">
-<<<<<<< HEAD
-                    @if (Config::get('app.locale') == 'en')
-                       @if ($order->payment_status == 'completed')
-=======
                         @if ($order->payment_status == 'completed')
->>>>>>> c42a79ae9da527b74c953d05e3ae4ed76f6534ef
                           <i class="la la-dot-circle-o success font-medium-1 mr-1"></i>
                           Completed
                           @elseif ($order->payment_status == 'inprocess')
@@ -176,30 +171,6 @@
                           <i class="la la-dot-circle-o danger font-medium-1 mr-1"></i>
                           Cancelled
                           @endif
-<<<<<<< HEAD
-                          
-                     @else
-                     
-                               @if ($order->payment_status == 'completed')
-                          <i class="la la-dot-circle-o success font-medium-1 mr-1"></i>
-                          أكتمل
-                          @elseif ($order->payment_status == 'inprocess')
-                          <i class="la la-dot-circle-o warning font-medium-1 mr-1"></i>
-                          تم الشحن 
-                          @elseif ($order->payment_status == 'shipped')
-                          <i class="la la-dot-circle-o warning font-medium-1 mr-1"></i>
-                          خرج للتوصيل
-
-                          @elseif ($order->payment_status == 'pending')
-                          <i class="la la-dot-circle-o warning font-medium-1 mr-1"></i>
-                          جاري الموافقة
-                          @elseif ($order->payment_status == 'cancelled')
-                          <i class="la la-dot-circle-o danger font-medium-1 mr-1"></i>
-                          الغاء
-                      @endif
-                     @endif
-=======
->>>>>>> c42a79ae9da527b74c953d05e3ae4ed76f6534ef
                       </td>
                       <td class="text-truncate"><a href="#">{{ $order->order_number }}</a></td>
                       <td class="text-truncate">
@@ -243,54 +214,6 @@
                         @endforeach
                       </td>
                       <td>
-<<<<<<< HEAD
-                        <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-                         
-                           @if ($order->payment_status == 'completed')
-                             <div class="progress-bar bg-gradient-x-success" role="progressbar" 
-                          style="width: 100%"
-                          aria-valuenow="100"
-                          @elseif ($order->payment_status == 'inprocess')
-                            <div class="progress-bar bg-gradient-x-success" role="progressbar" 
-                          style="width: 75%"
-                          aria-valuenow="75"
-                          @elseif ($order->payment_status == 'shipped')
-                            <div class="progress-bar bg-gradient-x-success" role="progressbar" 
-                          style="width: 50%"
-                          aria-valuenow="50"
-                          @elseif ($order->payment_status == 'pending')
-                            <div class="progress-bar bg-gradient-x-danger" role="progressbar" 
-                          style="width: 25%"
-                          aria-valuenow="25"
-                          @elseif ($order->payment_status == 'cancelled')
-                            <div class="progress-bar bg-gradient-x-danger" role="progressbar" 
-                          style="width: 0%"
-                          aria-valuenow="0"
-                          @endif
-                          
-                             aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                      </td>
-                      <td class="text-truncate"> 
-                       
-                     @foreach ( $order->product as $items_seller)
-                          
-                        @if($items_seller->vendor_id === Auth::guard('seller')->user()->id)
-                        @php
-                            $total_sum = array();
-                            if(!empty($items_seller->offer_price)){
-                              array_push($total_sum,$items_seller->offer_price);
-                            }else {
-                              array_push($total_sum,$items_seller->price);
-                            }
-                            
-                        @endphp
-                          {{ array_sum($total_sum)}} AED ({{ $order->pivot['quantity'] }})<br> 
-                          
-                          @endif
-                          @endforeach
-                     </td>
-=======
                        
                       @if ($order->payment_status == 'completed')
                       <div class="progress-bar bg-gradient-x-success" role="progressbar" 
@@ -335,7 +258,6 @@
                         {{ array_sum($total_sum)}} AED ({{ $order->pivot['quantity'] }})<br> 
                       </td>
                       
->>>>>>> c42a79ae9da527b74c953d05e3ae4ed76f6534ef
                     </tr>
                     @endforeach
                     @endforeach
